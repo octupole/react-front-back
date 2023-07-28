a87ee147f646f24abc94d7aa374a5fc30fa63673
octupole <33395395+octupole@users.noreply.github.com>
2023-07-28 13:07:17 +0200

    house-marketplace

---
+++ house-marketplace/firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDax6u8hC9jgAWVr1TbH4lTqzHYCaDkuKA",
+  authDomain: "hose-marketplace-app-74861.firebaseapp.com",
+  projectId: "hose-marketplace-app-74861",
+  storageBucket: "hose-marketplace-app-74861.appspot.com",
+  messagingSenderId: "764252480889",
+  appId: "1:764252480889:web:81b93c0340e4687bc0fba8"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(); 
